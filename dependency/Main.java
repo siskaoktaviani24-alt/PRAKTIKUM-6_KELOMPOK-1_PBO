c086d98eaa0acc65619d82b540d51acdf4f621a2
nihayahainun100-crypto <nihayahainun100@gmail.com>
2025-10-10 16:14:06 +0700

    2403010

---
+++ dependency/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Produk p1 = new Produk("Lip Tint", 45000);
+        Produk p2 = new Produk("Bedak Padat", 60000);
+        Produk p3 = new Produk("Maskara", 55000);
+
+        LaporanPenjualan laporan = new LaporanPenjualan("10 Oktober 2025");
+
+        laporan.tambahProdukTerjual(p1);
+        laporan.tambahProdukTerjual(p2);
+        laporan.tambahProdukTerjual(p3);
+
+        laporan.tampilkanDetailPenjualan();
+    }
+}
---
+++ dependency/Produk.java
@@ -0,0 +1,17 @@
+public class Produk {
+    private String nama;
+    private double harga;
+
+    public Produk(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+}
