4191f85e1ab85c54a3362f4023257fb3d59d9769
siskaoktaviani24-alt <siskaoktaviani24@student.polindra.ac.id>
2025-10-13 22:27:59 +0700

    2403004

--- generalization/Main.java
+++ generalization/Main.java
@@ -1,3 +1,4 @@
+// Main.java
 package generalization;
 
 import java.util.ArrayList;
@@ -5,13 +6,33 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Membuat objek-objek turunan ProdukMakeup
         Lipstick lipstick = new Lipstick("Maybelline Matte", 75000, "Merah Muda");
         Foundation foundation = new Foundation("Wardah Liquid", 120000, "Normal");
         Maskara maskara = new Maskara("L'Oreal Lash", 95000, true);
 
+        // Membuat daftar produk makeup
         List<ProdukMakeup> daftarProduk = new ArrayList<>();
         daftarProduk.add(lipstick);
         daftarProduk.add(foundation);
         daftarProduk.add(maskara);
+
+        // Menampilkan informasi produk menggunakan polymorphism
+        System.out.println("=== DAFTAR PRODUK MAKEUP ===");
+        for (ProdukMakeup produk : daftarProduk) {
+            System.out.println("Nama Produk : " + produk.getNamaProduk());
+            System.out.println("Harga       : Rp" + produk.getHarga());
+
+            // Memeriksa jenis produk dan menampilkan deskripsi sesuai kelasnya
+            if (produk instanceof Lipstick) {
+                System.out.println(((Lipstick) produk).getDeskripsi());
+            } else if (produk instanceof Foundation) {
+                System.out.println(((Foundation) produk).getDeskripsi());
+            } else if (produk instanceof Maskara) {
+                System.out.println(((Maskara) produk).getDeskripsi());
+            }
+
+            System.out.println("---------------------------");
+        }
     }
 }
